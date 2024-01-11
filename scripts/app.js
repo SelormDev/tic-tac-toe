cf6768caaa34eb831866fef4596068c4cabc20d7
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-01-11 19:39:10 +0000

    Add funtionality for player to make move

--- scripts/app.js
+++ scripts/app.js
@@ -10,26 +10,39 @@ Switch between players.
 
 (function initializeGame() {
   const gameBoard = {
-    1: ["O"],
-    2: [" "],
-    3: [" "],
-    4: ["X"],
-    5: ["O"],
-    6: ["X"],
-    7: ["X"],
-    8: [" "],
-    9: ["O"],
+    1: ["-"],
+    2: ["-"],
+    3: ["-"],
+    4: ["-"],
+    5: ["-"],
+    6: ["-"],
+    7: ["-"],
+    8: ["-"],
+    9: ["-"],
   };
 
-  const createPlayer = (name, symbol) => {
+  const createPlayer = (symbol) => {
+    const name = prompt("Enter your name");
     return { name, symbol };
   };
 
-  const displayGame = () => {
-    console.log(`
-        ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
-        ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]}
-        ${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}
-       `);
+  const displayBoard = () => {
+    let board = `${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
+${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]}
+${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}
+       `;
+    return board;
   };
+
+  //   const handlePlayerMove = () => {};
+  const makeMove = (player) => {
+    const spot = prompt(`Current Board\n${displayBoard()}\nChoose a spot.`);
+    gameBoard[spot] = player.symbol;
+  };
+
+  const switchPlayer = () => {};
+
+  const checkWin = () => {};
+
+  // return { getPlayerName };
 })();
