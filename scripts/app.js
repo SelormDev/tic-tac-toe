cb4dcf4a0317f3e6cceb04185ea8e30b510ed998
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-01-11 12:25:52 +0000

    Add displayGame function

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,35 @@
 "use strict";
 
-const gameBoard = {
-  1: [],
-  2: [],
-  3: [],
-  4: [],
-  5: [],
-  6: [],
-  7: [],
-  8: [],
-  9: [],
-};
+/**
+Create the game board.
+Implement the player factory function.
+Handle player moves.
+Check for a win or draw.
+Switch between players.
+ */
 
-const createPlayer = (name, symbol) => {
-  const playerName = name;
-  const playerSymbol = symbol;
+(function initializeGame() {
+  const gameBoard = {
+    1: ["O"],
+    2: [" "],
+    3: [" "],
+    4: ["X"],
+    5: ["O"],
+    6: ["X"],
+    7: ["X"],
+    8: [" "],
+    9: ["O"],
+  };
 
-  return { playerName, playerSymbol };
-};
+  const createPlayer = (name, symbol) => {
+    return { name, symbol };
+  };
+
+  const displayGame = () => {
+    console.log(`
+        ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
+        ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]}
+        ${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}
+       `);
+  };
+})();
