e5ee80334c3fb001358f9a7727918bf6566f2c2b
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-02-17 23:03:02 +0000

    Created game functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,150 +1,156 @@
 "use strict";
-const cells = document.querySelectorAll(".cell");
 
+// (function initializeGame() {
+const cells = document.querySelectorAll(".cell");
+const cell1 = document.getElementById("1").textContent;
+const cell2 = document.getElementById("2").textContent;
+const cell3 = document.getElementById("3").textContent;
+const cell4 = document.getElementById("4").textContent;
+const cell5 = document.getElementById("5").textContent;
+const cell6 = document.getElementById("6").textContent;
+const cell7 = document.getElementById("7").textContent;
+const cell8 = document.getElementById("8").textContent;
+const cell9 = document.getElementById("9").textContent;
+
+let players = [];
+let currentPlayer;
+
+const createPlayer = (symbol) => {
+  const name = prompt("Enter your name");
+  players.push({ name, symbol });
+  return { name, symbol };
+};
+
+// ------- Create Players -------
+(function createPlayers() {
+  while (players.length < 2) {
+    if (players.length < 1) {
+      createPlayer("X");
+    } else {
+      createPlayer("O");
+    }
+  }
+})();
+
+let gameBoard = {
+  1: cell1,
+  2: cell2,
+  3: cell3,
+  4: cell4,
+  5: cell5,
+  6: cell6,
+  7: cell7,
+  8: cell8,
+  9: cell9,
+};
+
+currentPlayer = players[0];
+
+// const makeMove = (player) => {
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
-    cell.textContent = "X";
+    if (cell.textContent !== "") {
+      alert("Spot already taken. Try again.");
+      // makeMove(player);
+    } else {
+      cell.textContent = currentPlayer.symbol;
+      gameBoard[cell.id] = currentPlayer.symbol;
+
+      if (!checkSpot()) {
+        alert("Draw");
+      } else if (checkForWin()) {
+        alert(`${currentPlayer.name} won!`);
+      }
+    }
   });
 });
+// };
 
-(function initializeGame() {
-  let gameBoard = {
-    1: ["-"],
-    2: ["-"],
-    3: ["-"],
-    4: ["-"],
-    5: ["-"],
-    6: ["-"],
-    7: ["-"],
-    8: ["-"],
-    9: ["-"],
-  };
-
-  let players = [];
-  let currentPlayer;
-
-  const createPlayer = (symbol) => {
-    const name = prompt("Enter your name");
-    players.push({ name, symbol });
-    return { name, symbol };
-  };
-
-  (function createPlayers() {
-    while (players.length < 2) {
-      if (players.length < 1) {
-        createPlayer("X");
-      } else {
-        createPlayer("O");
-      }
-    }
-  })();
-
-  currentPlayer = players[0];
-
-  const makeMove = (player) => {
-    const spot = prompt(
-      `Current Board\n${displayBoard()}\n${
-        currentPlayer.name
-      }'s turn. Your symbol is ${currentPlayer.symbol}\nChoose a spot.`
-    );
-    if (spot) {
-      if (gameBoard[spot][0] !== "-") {
-        alert("Spot already taken. Try again.");
-        makeMove(player);
-      } else {
-        gameBoard[spot] = player.symbol;
-      }
-    } else {
-      alert("Please enter a spot.");
-      makeMove(player);
-    }
-  };
-
-  const displayBoard = () => {
-    let board = `${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
+const displayBoard = () => {
+  let board = `${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
 ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]}
 ${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}
 `;
-    return board;
-  };
+  return board;
+};
 
-  const resetGameBoard = () => {
-    for (let board in gameBoard) {
-      gameBoard[board] = "-";
-    }
-  };
+const resetGameBoard = () => {
+  for (const [key] of Object.keys(gameBoard)) {
+    gameBoard[key] = "";
+  }
 
-  const switchPlayer = () => {
-    if (currentPlayer === players[0]) {
-      currentPlayer = players[1];
-    } else {
-      currentPlayer = players[0];
-    }
-  };
+  for (const cell of cells) {
+    cell.textContent = "";
+  }
+};
 
-  const checkForWin = (player) => {
-    if (
-      (player.symbol === gameBoard[1] &&
-        player.symbol === gameBoard[2] &&
-        player.symbol === gameBoard[3]) ||
-      (player.symbol === gameBoard[4] &&
-        player.symbol === gameBoard[5] &&
-        player.symbol === gameBoard[6]) ||
-      (player.symbol === gameBoard[7] &&
-        player.symbol === gameBoard[8] &&
-        player.symbol === gameBoard[9]) ||
-      (player.symbol === gameBoard[1] &&
-        player.symbol === gameBoard[4] &&
-        player.symbol === gameBoard[7]) ||
-      (player.symbol === gameBoard[2] &&
-        player.symbol === gameBoard[5] &&
-        player.symbol === gameBoard[8]) ||
-      (player.symbol === gameBoard[3] &&
-        player.symbol === gameBoard[6] &&
-        player.symbol === gameBoard[9]) ||
-      (player.symbol === gameBoard[1] &&
-        player.symbol === gameBoard[5] &&
-        player.symbol === gameBoard[9]) ||
-      (player.symbol === gameBoard[3] &&
-        player.symbol === gameBoard[5] &&
-        player.symbol === gameBoard[7])
-    ) {
-      return true;
-    }
-  };
+const switchPlayer = () => {
+  if (currentPlayer === players[0]) {
+    currentPlayer = players[1];
+  } else {
+    currentPlayer = players[0];
+  }
+};
 
-  const checkSpot = () => {
-    // for (let spot in gameBoard) {
-    //   if (gameBoard[spot][0] === "-") {
-    //     return true;
-    //   }
-    // }
-
-    for (let cell in cells) {
-      if (cell.textContent === "") {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  (function playGame() {
-    while (checkSpot()) {
-      makeMove(currentPlayer);
-      if (checkForWin(currentPlayer)) {
-        alert(`${currentPlayer.name} won!`);
-        console.log(`${currentPlayer.name} won!`);
-        break;
-      } else {
-        switchPlayer();
-      }
-    }
-    if (!checkSpot()) alert("Draw");
+const checkForWin = () => {
+  if (
+    (currentPlayer.symbol === gameBoard[1] &&
+      currentPlayer.symbol === gameBoard[2] &&
+      currentPlayer.symbol === gameBoard[3]) ||
+    (currentPlayer.symbol === gameBoard[4] &&
+      currentPlayer.symbol === gameBoard[5] &&
+      currentPlayer.symbol === gameBoard[6]) ||
+    (currentPlayer.symbol === gameBoard[7] &&
+      currentPlayer.symbol === gameBoard[8] &&
+      currentPlayer.symbol === gameBoard[9]) ||
+    (currentPlayer.symbol === gameBoard[1] &&
+      currentPlayer.symbol === gameBoard[4] &&
+      currentPlayer.symbol === gameBoard[7]) ||
+    (currentPlayer.symbol === gameBoard[2] &&
+      currentPlayer.symbol === gameBoard[5] &&
+      currentPlayer.symbol === gameBoard[8]) ||
+    (currentPlayer.symbol === gameBoard[3] &&
+      currentPlayer.symbol === gameBoard[6] &&
+      currentPlayer.symbol === gameBoard[9]) ||
+    (currentPlayer.symbol === gameBoard[1] &&
+      currentPlayer.symbol === gameBoard[5] &&
+      currentPlayer.symbol === gameBoard[9]) ||
+    (currentPlayer.symbol === gameBoard[3] &&
+      currentPlayer.symbol === gameBoard[5] &&
+      currentPlayer.symbol === gameBoard[7])
+  ) {
+    console.log(`${currentPlayer.name} won!`);
     resetGameBoard();
-  })();
+    return true;
+  }
 
-  const computerMove = () => Math.ceil(Math.random() * 9);
+  return false;
+};
 
-  return { displayBoard };
+const checkSpot = () => {
+  for (const [key, value] of Object.entries(gameBoard)) {
+    if (value === "") return true;
+  }
+
+  return false;
+};
+
+(function playGame() {
+  while (checkSpot()) {
+    makeMove(currentPlayer);
+    if (checkForWin(currentPlayer)) {
+      alert(`${currentPlayer.name} won!`);
+      console.log(`${currentPlayer.name} won!`);
+      break;
+    } else {
+      switchPlayer();
+    }
+  }
+  if (!checkSpot()) alert("Draw");
+  resetGameBoard();
 });
+
+const computerMove = () => Math.ceil(Math.random() * 9);
+
+// return { displayBoard };
+// });
